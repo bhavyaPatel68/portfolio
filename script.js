e9bfdaeb61400c00414b0e73d7ea1b6fddcabf04
bhavyaPatel68 <bp534532@gmail.com>
2026-06-24 21:33:35 -0500

    updated skills

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
     let car = document.getElementById("ferrari-car");
 
     window.addEventListener("scroll", () => {
-      const scrollY = window.scrollY;
+      let scrollY = window.scrollY;
 
       car.style.transform =
         `translateX(-${scrollY * 3}px) rotate(-${scrollY * 0.03}deg)`;
     });
+
+    
