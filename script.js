72c6fddc3665633b3d9fc2109581904e9c71674b
bhavyaPatel68 <bp534532@gmail.com>
2026-06-25 15:20:30 -0500

    bottom car

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-    let car = document.getElementById("ferrari-car");
+let car = document.getElementById("ferrari-car");
+let invertCar = document.getElementById("invert-ferrari-car");
 
-    window.addEventListener("scroll", () => {
-      let scrollY = window.scrollY;
+function handleScroll() {
+  let scrollY = window.scrollY;
+  car.style.transform =
+    `translateX(-${scrollY * 3}px) rotate(-${scrollY * 0.03}deg)`;
 
-      car.style.transform =
-        `translateX(-${scrollY * 3}px) rotate(-${scrollY * 0.03}deg)`;
-    });
+  const bottomCarLoc = invertCar.getBoundingClientRect();
+  const windowHeight = window.innerHeight;
+
+  let progress = (windowHeight - bottomCarLoc.top) / (windowHeight * 0.6);
+  progress = Math.min(Math.max(progress, 0), 1);
+
+  const startOffset = 1200;
+  const xOffset = -startOffset * (1 - progress);
+
+  invertCar.style.transform = `translateX(${xOffset}px)`;
+}
+
+window.addEventListener("scroll", handleScroll);
+window.addEventListener("load", handleScroll);
